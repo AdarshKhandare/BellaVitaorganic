87b997bb8790f583f671a3885b96bee90d99a88b
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-04-01 18:52:15 +0530

    card

---
+++ js/card.js
@@ -0,0 +1,19 @@
+function openWindow(co){
+   co.style.display="flex"
+}
+function closeWindow(co){
+    co.style.display="none"
+}
+
+function openCart(coc){
+    coc.style.display="flex"
+}
+function closeCart(coc){
+    coc.style.display="none"
+}
+
+ // login part
+
+
+
+export {openWindow,closeWindow,openCart,closeCart}

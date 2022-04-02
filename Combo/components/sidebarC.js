6b5cd9ea2a7ca0fc41b79cc4b2f1a8aaf496a71f
AdarshKhandare <kingadarsh313@gmail.com>
2022-04-02 17:12:22 +0530

    linking

--- Combo/components/sidebarC.js
+++ Combo/components/sidebarC.js
@@ -3,26 +3,26 @@ function sidebar(){
     <div id="category">CATEGORIES</div>
     <ul class="nav flex-column" id="nav_accordion">
         <li class="nav-item">
-            <a class="nav-link" href="#"> All Products </a>
-            <a class="nav-link" href="#"> Best Seller </a>
+            <a class="nav-link" href="../AnandData/allProducts.html"> All Products </a>
+            <a class="nav-link" href="/AnandData/bestSellers.html"> Best Seller </a>
             <a class="nav-link" href="/Build-A-Box/bab.html"> Build A Box </a>
         </li>
         <li class="nav-item has-submenu">
-            <div class="nav-link catelog" style="display: flex; justify-content: space-between;" ><a href="#"> Offers</a><i class="fa-solid fa-angle-down"></i> </div>
+            <div class="nav-link catelog" style="display: flex; justify-content: space-between;" ><a href=""> Offers</a><i class="fa-solid fa-angle-down"></i> </div>
             <ul class="submenu collapse">
-                <li><a class="nav-link" href="#">All Offers </a></li>
-                <li><a class="nav-link" href="#">Collaborations </a></li>
+                <li><a class="nav-link" href="../../../ADITI/offers/offer.html">All Offers </a></li>
+                <li><a class="nav-link" href="../../../ADITI/offers/colb.html">Collaborations </a></li>
             </ul>
         </li>
         <li class="nav-item has-submenu">
             <div class="nav-link catelog" style="display: flex; justify-content: space-between;" ><a href="#"> By Category</a><i class="fa-solid fa-angle-down"></i> </div>
             <ul class="submenu collapse">
-                <li><a class="nav-link" href="#">Skin Care </a></li>
-                <li><a class="nav-link" href="#">Hair Care </a></li>
-                <li><a class="nav-link" href="#">Face Care </a></li>
+                <li><a class="nav-link" href="../sagar/dryskin.html">Skin Care </a></li>
+                <li><a class="nav-link" href="../sagar/hair.html">Hair Care </a></li>
+                <li><a class="nav-link" href="../sagar/face.html">Face Care </a></li>
                 <li><a class="nav-link" href="#">Body Care </a></li>
-                <li><a class="nav-link" href="#">Essential Oils </a></li>
-                <li><a class="nav-link" href="#">Perfumes </a></li>
+                <li><a class="nav-link" href="../sagar/essential.html">Essential Oils </a></li>
+                <li><a class="nav-link" href="../sagar/perfumes.html">Perfumes </a></li>
                 <li><a class="nav-link" href="#">Natural Soaps </a></li>
                 <li><a class="nav-link" href="#">Hair Combs </a></li>
             </ul>
@@ -72,11 +72,11 @@ function sidebar(){
         <li class="nav-item has-submenu">
             <div class="nav-link catelog" style="display: flex; justify-content: space-between;" ><a href="#"> More</a><i class="fa-solid fa-angle-down"></i> </div>
             <ul class="submenu collapse">
-                <li><a class="nav-link" href="#">Natural Soaps </a></li>
-                <li><a class="nav-link" href="#">SELFIE SWITCH </a></li>
-                <li><a class="nav-link" href="#">Gift Cards </a></li>
+                <li><a class="nav-link" href="../../../ADITI/MORE/nature_soap/natural_soap.html">Natural Soaps </a></li>
+                <li><a class="nav-link" href="../../../ADITI/MORE/selfie.html">SELFIE SWITCH </a></li>
+                <li><a class="nav-link" href="../../../ADITI/MORE/gift.html">Gift Cards </a></li>
                 <li><a class="nav-link" href="#">Blog </a></li>
-                <li><a class="nav-link" href="#">Salon </a></li>
+                <li><a class="nav-link" href="../../../ADITI/MORE/salon.html">Salon </a></li>
                 <li><a class="nav-link" href="#">Order Tracking </a></li>
                 <li><a class="nav-link" href="#">Contact us </a></li>
                 <li><a class="nav-link" href="#">Shipping Policy </a></li>
